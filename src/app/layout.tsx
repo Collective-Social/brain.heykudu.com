2b58b26c1269c395790a4349f40cfe7f65e74a61
Duncan Luke <duncanluke@Duncans-Mac-mini.local>
2026-07-03 10:47:18 +0200

    Chore: Add SessionRewind tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,24 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script id="session-rewind" strategy="afterInteractive">
+          {`
+            !function (o) {
+                var w = window;
+                w.SessionRewindConfig = o;
+                var f = document.createElement("script");
+                f.async = 1, f.crossOrigin = "anonymous",
+                  f.src = "https://rec.sessionrewind.com/srloader.js";
+                var g = document.getElementsByTagName("head")[0];
+                g.insertBefore(f, g.firstChild);
+              }({
+                apiKey: 'ttXErZctR3aSnUGvFDjGq7DHF0LkXMql4U38GMPc',
+                startRecording: true,
+              });
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col font-sans bg-[#0A0714]">
         <div className="flex-1 flex flex-col">{children}</div>
       </body>
